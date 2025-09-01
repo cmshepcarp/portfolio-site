dac99ba18e8a1d0da5554c66f7c14a4d57fda55f
cmshepcarp <cmshepcarp@gmail.com>
2025-08-31 22:23:08 -0700

    fix: add @vercel/analytics dependency and update config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-﻿import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+﻿const nextConfig: NextConfig = {
   experimental: {
+    
     allowedDevOrigins: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "http://localhost:3001",
-      "http://192.168.1.229:3001"
+      "http://192.168.1.229:3001",
     ],
   },
 };
-
 export default nextConfig;
